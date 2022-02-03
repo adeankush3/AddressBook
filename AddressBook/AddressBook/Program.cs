e716fba73e5d9091313c2605aee8200436cdf251
adeankush3 <adeankush3@gmail.com>
2022-02-03 15:59:44 +0530

    [Ankush] Adress Book Problem Solving Create Name, Address etc

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Address Book Program");
         }
     }
 }
