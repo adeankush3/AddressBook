75f04ae84d81dd879893a618582a7e74ad57b9b0
adeankush3 <adeankush3@gmail.com>
2022-02-03 16:32:19 +0530

    [Ankush] Adress Book Problem Solving Create Name, Address etc

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -11,6 +11,25 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Create create = new Create();
+            Console.WriteLine("Enter First Name");
+            create.firstName=Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            create.lastName = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            create.email = Console.ReadLine();
+            Console.WriteLine("Enter Phone");
+            create.phone = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            create.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            create.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            create.state = Console.ReadLine();
+            Console.WriteLine("Enter ZIP");
+            create.zip = Convert.ToInt32(Console.ReadLine());
+
+
         }
     }
 }
